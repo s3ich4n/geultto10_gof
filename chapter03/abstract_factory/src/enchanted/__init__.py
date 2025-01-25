4cf7c18eba9a049aa1d933398ca89b45b1a7ba54
s3ich4n <s3ich4n@gmail.com>
2025-01-26 04:30:16 +0900

    feat(abstract_factory): 추상 팩토리 추가 (2) - 인챈트된 방 생성 케이스에 대해

---
+++ chapter03/abstract_factory/src/enchanted/__init__.py
@@ -0,0 +1,3 @@
+from .door import EnchantedDoor
+from .room import EnchantedRoom
+from .wall import EnchantedWall
---
+++ chapter03/abstract_factory/src/enchanted/room.py
@@ -0,0 +1,6 @@
+from src.room import Room
+
+
+class EnchantedRoom(Room):
+    def enter(self):
+        print("마법의 방에 들어옴")
